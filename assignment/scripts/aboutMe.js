9452d4ed4df7494584964d1c371d47d9e613583c
Tou Xiong <tlxiong.prime@outlook.com>
2021-07-24 11:36:17 -0500

    week 2 pt.1 line 26 finsihed

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -13,17 +13,17 @@ let luckyNumber = 11;
 // 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-
+console.log( 'My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = 'Pho';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 1;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2;
 // 11 - Add two pets to your `pets` variable
-
+pets = 3;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
